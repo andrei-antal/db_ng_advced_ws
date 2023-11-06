7445287bb3305fba28225a736ec671d5ccfabcc8
Andrei Antal <antal.andrei@icloud.com>
2023-11-06 22:23:10 +0200

    done challenge 15

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { Route, provideRouter } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { isAuthenticatedGuard } from './movies/guards/is-authenticated.guard';
+import { authInterceptor } from './movies/interceptors/auth.interceptor';
 
 const routes: Route[] = [
   { path: '', component: HomeComponent },
@@ -15,5 +16,8 @@ const routes: Route[] = [
 ];
 
 export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(HttpClientModule), provideRouter(routes)],
+  providers: [
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(routes),
+  ],
 };
